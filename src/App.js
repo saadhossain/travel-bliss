7034b88ba58a2711083890d76aea1405c95a160b
saadhossain <mshsarkar@gmail.com>
2022-10-22 18:10:17 +0600

    Hotels added on server and header added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { routes } from './Routers/Routes';
 function App() {
   const router = routes;
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router}>
 
       </RouterProvider>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../assests/airplane.png';
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='bg-tbliss text-white py-2'>
+            <div className='w-10/12 mx-auto flex justify-between items-center'>
+                <div className='flex items-center gap-2'>
+                    <img src={logo} alt='Branding' className='h-10 w-10' />
+                    <h1 className='text-3xl font-bold'>TravelBliss</h1>
+                </div>
+                <div>
+                    <ul className='font-semibold md:flex gap-4'>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/destination'>Destinations</Link></li>
+                        <li><Link to='/booking'>Booking</Link></li>
+                        <li><Link to='/blogs'>Blog</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
