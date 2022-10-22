3703713ce723ac40049b54e15d34a50f70cb1d2c
saadhossain <mshsarkar@gmail.com>
2022-10-22 22:54:37 +0600

    Github login register enabled

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,16 +1,21 @@
 import React, { createContext } from 'react';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
 import app from '../Firebase/firebase.config'
 
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
     const auth = getAuth(app);
     const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+    //Login/Register with Google
     const googleSignIn = () => {
         return signInWithPopup(auth, googleProvider);
     }
-
-    const userInfo = {googleSignIn}
+    //Login with Github
+    const githubLogin = () => {
+        return signInWithPopup(auth, githubProvider)
+    }
+    const userInfo = {googleSignIn, githubLogin}
     return (
         <AuthContext.Provider value={userInfo}>
             {children}
