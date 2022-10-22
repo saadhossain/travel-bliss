6cbd583eec2d9c7276821ea222382a45caf9871f
saadhossain <mshsarkar@gmail.com>
2022-10-22 23:02:12 +0600

    Logged in user set to state and live server updated

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import {getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithPopup} from 'firebase/auth'
 import app from '../Firebase/firebase.config'
 
 export const AuthContext = createContext();
@@ -7,6 +7,8 @@ const AuthProvider = ({children}) => {
     const auth = getAuth(app);
     const googleProvider = new GoogleAuthProvider();
     const githubProvider = new GithubAuthProvider();
+    //Set user to the state
+    const [user, setUser] = useState()
     //Login/Register with Google
     const googleSignIn = () => {
         return signInWithPopup(auth, googleProvider);
@@ -15,7 +17,17 @@ const AuthProvider = ({children}) => {
     const githubLogin = () => {
         return signInWithPopup(auth, githubProvider)
     }
-    const userInfo = {googleSignIn, githubLogin}
+    //Get the user data
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+        })
+        return ()=> unSubscribe();
+    }, [auth])
+    
+    const userInfo = {user, googleSignIn, githubLogin}
+
+
     return (
         <AuthContext.Provider value={userInfo}>
             {children}
--- src/Routers/Routes.js
+++ src/Routers/Routes.js
@@ -4,24 +4,24 @@ import Home from "../Components/Home/Home";
 import Hotels from "../Components/Hotels/Hotels";
 import Login from "../Components/Login/Login";
 import Register from "../Components/Register/Register";
-import Main from '../Layout/Main'
+import Main from '../Layout/Main';
 export const routes = createBrowserRouter([
     {
-        path:'/', element:<Main></Main>,
-        children:[
+        path: '/', element: <Main></Main>,
+        children: [
             {
-                path:'/',
-                loader: () => fetch('http://localhost:5000/destinations'),
-                element:<Home></Home>
+                path: '/',
+                loader: () => fetch('https://travel-bliss-server.vercel.app/destinations'),
+                element: <Home></Home>
             },
             {
-                path:'/hotels/:id',
-                loader: ({params})=> fetch(`http://localhost:5000/hotels/${params.id}`),
-                element:<Hotels></Hotels>
+                path: '/hotels/:id',
+                loader: ({ params }) => fetch(`https://travel-bliss-server.vercel.app/hotels/${params.id}`),
+                element: <Hotels></Hotels>
             },
-            {path:'/login', element:<Login></Login>},
-            {path:'/register', element:<Register></Register>},
-            {path:'/booking', element:<Booking></Booking>}
+            { path: '/login', element: <Login></Login> },
+            { path: '/register', element: <Register></Register> },
+            { path: '/booking', element: <Booking></Booking> }
         ]
     }
 ])
